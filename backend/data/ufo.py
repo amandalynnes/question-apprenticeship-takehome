2bf9a4659e93e41816e89d7bd3d4cc11835ea2b0
Amanda Simmons <carpe737diem@gmail.com>
2021-05-21 14:46:46 -0400

    added pseudocode/ actionable steps to take to accomplish the task

--- backend/data/ufo.py
+++ backend/data/ufo.py
@@ -100,3 +100,26 @@ x = ["""                 .
 """]
 
 print(x[0])
+
+# Breakdown/ Overview
+# create a program that allows the user to guess the letters of a 
+# selected word represented by underscores
+# if your guess is right the letter is revealed
+# if you guess incorrectly the next string in list 'x' will be rendered 
+# causing the person to float up a level
+# this continues for each guess until you uncover the word or the last string in 
+# list 'x' is printed aka the person is inside of the spaceship
+
+# Actionable Steps
+# Pick and display a random word from nouns.txt
+# hide the letters with undersores
+# allow for user input of letters in the terminal(guesses)
+# do membership check to see if the guess is in the hidden word
+#   if so reveal all occurences of the letter in the word
+#   if not render the next string in the list 'x' to cause the person to float
+#   up one level along with an encouraging message from messages.txt
+# repeat this process untill the word is revealed (guessed) or last string in 
+# list 'x' is printed aka the person is inside of the spaceship
+
+
+
